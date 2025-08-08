22af329d9fac32ad18c8e037b6b7a9164ce6ca79
nani1nani <nani121323@gmail.com>
2025-08-08 21:34:34 +0530

    server intialized with socket

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const cors = require("cors");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+const server = http.createServer(app);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
 const port = 5000;
 
@@ -8,6 +19,6 @@ app.use("/", (req, res) => {
   res.send("hello developer");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("server started");
 });
